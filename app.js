f1d3d1aeaf514d376a53444e116ce37e8e9c4a02
DEEPAKRAJAN07 <kdeepakrajan2003@gmail.com>
2024-01-26 20:05:18 +0530

    firstcommit

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // or specify the allowed origin(s)
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
+
 const bcrypt = require("bcrypt");
 
 app.get("/", cors(), (req, res) => {});
