473cbafb35c2bfee38ff46f2e1fa8f38fddf7850
DEEPAKRAJAN07 <kdeepakrajan2003@gmail.com>
2024-01-26 20:31:33 +0530

    firstcommit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const collection = require("./mongo");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
